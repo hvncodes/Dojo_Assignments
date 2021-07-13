64d94a212774a47a97a4db8fe100bae3b8f4fbdb
John Nguyen <johnhvn94@gmail.com>
2021-07-13 03:56:44 -0700

    More Template Rendering

--- Python/flask/fundamentals/hello_flask/hello.py
+++ Python/flask/fundamentals/hello_flask/hello.py
@@ -5,6 +5,18 @@ def hello_world():
     # Instead of returning a string, 
     # we'll return the result of the render_template method, passing in the name of our HTML file
     return render_template('index.html', phrase="hello", times=5)
+
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
 
