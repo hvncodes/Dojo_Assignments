08c9fa7096b5236fa3e6552122d7b794b5d678d0
John Nguyen <johnhvn94@gmail.com>
2021-07-18 05:50:18 -0700

    now how did that get there?

--- Python/flask_mysql/db_connection/users/flask_app/controllers/users.py
+++ Python/flask_mysql/db_connection/users/flask_app/controllers/users.py
@@ -1,4 +1,4 @@
-from flask import render_template,redirect,request,session,flash
+from flask import render_template,redirect,request,session
 from flask_app import app
 from flask_app.models.user import User
 
