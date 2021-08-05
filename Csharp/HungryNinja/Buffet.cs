8a6fc68d005fec60085b54b65154bf1cbe8e5781
John Nguyen <johnhvn94@gmail.com>
2021-08-05 06:26:51 -0700

    minor indent

--- Csharp/HungryNinja/Buffet.cs
+++ Csharp/HungryNinja/Buffet.cs
@@ -4,30 +4,30 @@ using System.Collections.Generic;
 namespace HungryNinja
 {
     class Buffet
-{
-    public List<Food> Menu;
-     
-    //constructor
-    public Buffet()
     {
-        Menu = new List<Food>()
-        {   // Name, Calories, IsSpicy, IsSweet
-            new Food("Example", 0, true, true),
-            new Food("Pizza", 2000, false, true),
-            new Food("Burger", 500, false, false),
-            new Food("Cheetos", 200, true, false),
-            new Food("Apple Juice", 100, false, true),
-            new Food("Boba Ice Cream", 300, false, true),
-            new Food("Steak", 400, false, false)
-        };
+        public List<Food> Menu;
+         
+        //constructor
+        public Buffet()
+        {
+            Menu = new List<Food>()
+            {   // Name, Calories, IsSpicy, IsSweet
+                new Food("Example", 0, true, true),
+                new Food("Pizza", 2000, false, true),
+                new Food("Burger", 500, false, false),
+                new Food("Cheetos", 200, true, false),
+                new Food("Apple Juice", 100, false, true),
+                new Food("Boba Ice Cream", 300, false, true),
+                new Food("Steak", 400, false, false)
+            };
+        }
+         
+        public Food Serve()
+        {
+            Random rng = new Random();
+            return Menu[rng.Next(Menu.Count)];
+        }
     }
-     
-    public Food Serve()
-    {
-        Random rng = new Random();
-        return Menu[rng.Next(Menu.Count)];
-    }
-}
 
 
 }
