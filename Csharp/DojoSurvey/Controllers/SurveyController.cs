9e47883022bab9efd1ad46defc35631836e4d57a
John Nguyen <johnhvn94@gmail.com>
2021-08-09 20:48:03 -0700

    ViewModel Fun

--- Csharp/DojoSurvey/Controllers/SurveyController.cs
+++ Csharp/DojoSurvey/Controllers/SurveyController.cs
@@ -1,6 +1,8 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using System.Globalization;
+using DojoSurvey.Models;
+using System.Collections.Generic;
 
 namespace DojoSurvey
 {
@@ -11,11 +13,12 @@ namespace DojoSurvey
         {
             DateTime dt = DateTime.Now;
             ViewBag.dt = dt;
-            return View("Index");
+            string header = "The current time and date:";
+            return View("Index", header);
         }
 
         [HttpPost("process")]
-        public IActionResult Method(string Name, string Location, string Language, string Comment)
+        public IActionResult Result(string Name, string Location, string Language, string Comment)
         {
             ViewBag.Name = Name;
             ViewBag.Location = Location;
@@ -24,6 +27,49 @@ namespace DojoSurvey
             return View("Result");
         }
 
+        [HttpGet("numbers")]
+        public ViewResult Numbers()
+        {
+            int[] numbers = new int[]
+            {
+                1, 2, 3, 10, 43, 5
+            };
+            return View("Numbers", numbers);
+        }
 
+        [HttpGet("user")]
+        public IActionResult SoleUser()
+        {
+            User someUser = new User()
+            {
+                LastName = "Ever",
+                FirstName = "Greatest"
+            };
+            return View("User", someUser);
+        }
+
+        [HttpGet("users")]
+        public IActionResult Users()
+        {
+            User someUser = new User()
+            {
+                FirstName = "Thomas",
+                LastName = "Anderson"
+            };
+            User otherUser = new User()
+            {
+                FirstName = "Agent",
+                LastName = "Smith"
+            };
+            User anotherUser = new User()
+            {
+                FirstName = "Morpheus"
+            };
+            List<User> users = new List<User>()
+            {
+                someUser, otherUser, anotherUser
+            };
+            return View("Users", users);
+        }
     }
 }
