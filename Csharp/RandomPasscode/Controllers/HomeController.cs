9aa49878f52409198308ca2fce88d31df6f09617
John Nguyen <johnhvn94@gmail.com>
2021-08-11 18:04:10 -0700

    Added Generate Route and Session checks to Index

--- Csharp/RandomPasscode/Controllers/HomeController.cs
+++ Csharp/RandomPasscode/Controllers/HomeController.cs
@@ -18,11 +18,38 @@ namespace RandomPasscode.Controllers
         {
             _logger = logger;
         }
-        
+
         [HttpGet("")]
         public IActionResult Index()
         {
+            if (HttpContext.Session.GetInt32("Count") == null)
+            {
+                HttpContext.Session.SetInt32("Count", 0);
+                Generate();
+            }
+
             return View();
+        }
+
+        public IActionResult Generate()
+        {
+            int? current = HttpContext.Session.GetInt32("Count");
+            HttpContext.Session.SetInt32("Count", (int)current + 1);
+            string alphanumeral = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            Random rng = new Random(); //rng.Next(alphanumeral.Length);
+            char[] passcode = new char[14];
+            for (int i = 0; i < passcode.Length; i++)
+            {
+                int idxRngAlphaNum = rng.Next(alphanumeral.Length);
+                // Console.WriteLine(idxRngAlphaNum);
+                // Console.WriteLine(alphanumeral[idxRngAlphaNum]);
+                // Console.WriteLine("----------------------");
+                passcode[i] = alphanumeral[idxRngAlphaNum];
+            }
+            // Console.WriteLine(passcode);
+            // Console.WriteLine(new string(passcode));
+            HttpContext.Session.SetString("Passcode", new string(passcode));
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
