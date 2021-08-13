6547baf6b8d9371ebc301b43df905014631163b0
John Nguyen <johnhvn94@gmail.com>
2021-08-13 14:21:02 -0700

    Updated startup

--- Csharp/CRUDelicious/Startup.cs
+++ Csharp/CRUDelicious/Startup.cs
@@ -24,14 +24,32 @@ namespace CRUDelicious
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-            services.AddDbContext<MyContext>(options => options.UseMySql (Configuration["DBInfo:ConnectionString"]));
+            // services.AddControllersWithViews();
+            // services.AddDbContext<MyContext>(options => options.UseMySql (Configuration["DBInfo:ConnectionString"]));
+            // services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddDbContext<MyContext>(options => options.UseMySql(
+                Configuration["DBInfo:ConnectionString"],
+                ServerVersion.FromString("8.0.23-mysql")));
+        
+            // to access session directly from view, corresponds with: @using Microsoft.AspNetCore.Http in Views/_ViewImports.cshtml
+            services.AddHttpContextAccessor();
+            services.AddSession();
             services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // app.UseRouting();
+
+            // app.UseAuthorization();
+
+            // app.UseEndpoints(endpoints =>
+            // {
+            //     endpoints.MapControllerRoute(
+            //         name: "default",
+            //         pattern: "{controller=Home}/{action=Index}/{id?}");
+            // });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -40,18 +58,17 @@ namespace CRUDelicious
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            // css, js, and image files can now be added to wwwroot folder
             app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            app.UseSession();
+            app.UseMvc(routes =>
             {
-                endpoints.MapControllerRoute(
+                routes.MapRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
+
         }
     }
 }
