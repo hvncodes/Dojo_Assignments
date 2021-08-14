d615715b9d4f6c20b56133f9db3474e2be95859f
John Nguyen <johnhvn94@gmail.com>
2021-08-13 18:20:09 -0700

    Added validations to Dish Model

--- Csharp/CRUDelicious/Models/Dish.cs
+++ Csharp/CRUDelicious/Models/Dish.cs
@@ -6,10 +6,29 @@ namespace CRUDelicious.Models
     {
         [Key]
         public int DishId { get; set; }
+
+        [Required(ErrorMessage = "is required.")]
+        [MinLength(2, ErrorMessage = "must be at least 2 characters.")]
+        [Display(Name = "Name of Dish")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "is required.")]
+        [MinLength(2, ErrorMessage = "must be at least 2 characters.")]
+        [Display(Name = "Chef's Name")]
         public string Chef { get; set; }
-        public int Tastiness { get; set; }
-        public int Calories { get; set; }
+
+        [Required(ErrorMessage = "is required.")]
+        [Range(1, 5, ErrorMessage = "must be at between 1 and 5.")]
+        // [Display(Name = "Tastiness")]
+        public int? Tastiness { get; set; }
+
+        [Required(ErrorMessage = "is required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "must be at least 0 calories.")]
+        [Display(Name = "# of Calories")]
+        public int? Calories { get; set; }
+
+        [Required(ErrorMessage = "is required.")]
+        [MinLength(2, ErrorMessage = "must be at least 2 characters.")]
         public string Description { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
