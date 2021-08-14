19f8a132920c3fd2c0da951cc8211310f80e47ef
John Nguyen <johnhvn94@gmail.com>
2021-08-13 18:19:47 -0700

    Modified URL routing, added new routes: New and Create and other minor changes.

--- Csharp/CRUDelicious/Controllers/HomeController.cs
+++ Csharp/CRUDelicious/Controllers/HomeController.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CRUDelicious.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace CRUDelicious.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        // private readonly ILogger<HomeController> _logger;
 
         // public HomeController(ILogger<HomeController> logger)
         // {
@@ -31,6 +32,8 @@ namespace CRUDelicious.Controllers
         {
             // List<Dish> AllDishes = _context.Dishes.ToList();
             // return View("Index", AllDishes);
+            // HttpContext.Session.SetInt32("userID", 1);
+            // Console.WriteLine();
             List<Dish> RecentFiveDishes = _context.Dishes
                 .OrderByDescending(d => d.CreatedAt)
                 .Take(5)
@@ -38,13 +41,46 @@ namespace CRUDelicious.Controllers
             return View("Index", RecentFiveDishes);
         }
 
-        [HttpGet("/{dishId}")]
+        [HttpGet("dishes/{dishId}")]
         public IActionResult OneDish(int dishId)
         {
             Dish OneDish = _context.Dishes.SingleOrDefault(d => d.DishId == dishId);;
             return View("OneDish", OneDish);
         }
 
+        [HttpGet("dishes/new")]
+        public IActionResult New()
+        {
+            return View("New");
+        }
+
+        [HttpPost("dishes/process")]
+        public IActionResult Create(Dish newDish)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(newDish);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("New");
+        }
+
+        [HttpGet("dishes/{dishId}/edit")]
+        public IActionResult Edit(int dishId)
+        {
+            Dish OneDish = _context.Dishes.SingleOrDefault(d => d.DishId == dishId);;
+            return View("Edit", OneDish);
+        }
+
+        [HttpPost("dishes/{dishId}/update")]
+        public IActionResult update(int dishId)
+        {
+            // take something in as params
+            // do stuff
+            // db save
+            return RedirectToAction("Edit", dishId);
+        }
 
         public IActionResult Privacy()
         {
