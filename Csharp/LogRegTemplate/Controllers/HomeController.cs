4c7ebb280e869417f373f34253903cc39db1db4e
John Nguyen <johnhvn94@gmail.com>
2021-08-17 19:53:48 -0700

    Added Login Routes to Controller

--- Csharp/LogRegTemplate/Controllers/HomeController.cs
+++ Csharp/LogRegTemplate/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using LogRegTemplate.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
 
 namespace LogRegTemplate.Controllers
 {
@@ -16,11 +18,115 @@ namespace LogRegTemplate.Controllers
         {
             db = context;
         }
-        
+
+        private int? uid
+        {
+            get
+            {
+                return HttpContext.Session.GetInt32("UserId");
+            }
+        }
+
+        private bool isLoggedIn
+        {
+            get
+            {
+                return uid != null;
+            }
+        }
+
         [HttpGet("")]
         public IActionResult Index()
         {
+            if (isLoggedIn)
+            {
+                return RedirectToAction("Success");
+            }
             return View();
+        }
+
+        [HttpPost("/login")]
+        public IActionResult Login(LoginUser loginUser)
+        {
+            if (ModelState.IsValid == false)
+            {
+                // Display error messages.
+                return View("Index");
+            }
+
+            User dbUser = db.Users.FirstOrDefault(user => user.Email == loginUser.LoginEmail);
+
+            if (dbUser == null)
+            {
+                /*
+                Usually you don't want to reveal specifics, like was the email
+                or password invalid because of phishing. But for testing it's
+                easier to see what actually went wrong with specific errors.
+                */
+                ModelState.AddModelError("LoginEmail", "Email not found.");
+                return View("Index");
+            }
+
+            PasswordHasher<LoginUser> hasher = new PasswordHasher<LoginUser>();
+            PasswordVerificationResult pwCompareResult = hasher.VerifyHashedPassword(loginUser, dbUser.Password, loginUser.LoginPassword);
+
+            if (pwCompareResult == 0)
+            {
+                // Don't reveal this info, this is just for testing.
+                ModelState.AddModelError("LoginPassword", "Invalid Password");
+                return View("Index");
+            }
+
+            HttpContext.Session.SetInt32("UserId", dbUser.UserId);
+            HttpContext.Session.SetString("FirstName", dbUser.FirstName);
+            return RedirectToAction("Success");
+        }
+
+        [HttpPost("/register")]
+        public IActionResult Register(User newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                // If email already exists.
+                if (db.Users.Any(u => u.Email == newUser.Email))
+                {
+                    ModelState.AddModelError("Email", "is taken.");
+                }
+            }
+
+            // If any above custom errors were added, ModelState would now be invalid.
+            if (ModelState.IsValid == false)
+            {
+                // So error messages are displayed.
+                return View("Index");
+            }
+
+            PasswordHasher<User> hasher = new PasswordHasher<User>();
+            newUser.Password = hasher.HashPassword(newUser, newUser.Password);
+
+            db.Users.Add(newUser);
+            db.SaveChanges();
+
+            HttpContext.Session.SetInt32("UserId", newUser.UserId);
+            HttpContext.Session.SetString("FirstName", newUser.FirstName);
+            return RedirectToAction("Success");
+        }
+
+        [HttpGet("/success")]
+        public IActionResult Success()
+        {
+            if (!isLoggedIn)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpPost("/logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
