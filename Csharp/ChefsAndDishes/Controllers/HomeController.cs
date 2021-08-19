8a4b680df48bd29c76d34a0d2f5565a15cfc12cd
John Nguyen <johnhvn94@gmail.com>
2021-08-18 19:08:06 -0700

    New Chef and Create Chef routes

--- Csharp/ChefsAndDishes/Controllers/HomeController.cs
+++ Csharp/ChefsAndDishes/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ChefsAndDishes.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace ChefsAndDishes.Controllers
 {
@@ -20,9 +23,39 @@ namespace ChefsAndDishes.Controllers
         [HttpGet("")]
         public IActionResult Index()
         {
-            return View();
+            List<Chef> Chefs = db.Chefs
+            .Include(chef => chef.Dishes)
+            .ToList();
+            return View("Index", Chefs);
         }
 
+        [HttpGet("/new")]
+        public IActionResult New()
+        {
+            return View("New");
+        }
+
+        [HttpPost("/new/process")]
+        public IActionResult Create(Chef newChef)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View("New");
+            }
+            if (newChef.DateOfBirth >= DateTime.Now)
+            {
+                ModelState.AddModelError("DateOfBirth", "must be a past date.");
+                return View("New");
+            }
+            if (DateTime.Now.Year - newChef.DateOfBirth.Year < 18)
+            {
+                ModelState.AddModelError("DateOfBirth", "must be 18 y/o or older");
+                return View("New");
+            }
+            db.Add(newChef);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
         public IActionResult Privacy()
         {
             return View();
