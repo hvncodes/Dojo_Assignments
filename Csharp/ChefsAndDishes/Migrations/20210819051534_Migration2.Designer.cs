eb03dc19f5df22cb3224b45e20135f54d58e946d
John Nguyen <johnhvn94@gmail.com>
2021-08-19 01:43:25 -0700

    Migrations

--- Csharp/ChefsAndDishes/Migrations/20210819051534_Migration2.Designer.cs
+++ Csharp/ChefsAndDishes/Migrations/20210819051534_Migration2.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using ChefsAndDishes.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ChefsAndDishes.Migrations
 {
     [DbContext(typeof(ChefsAndDishesContext))]
-    partial class ChefsAndDishesContextModelSnapshot : ModelSnapshot
+    [Migration("20210819051534_Migration2")]
+    partial class Migration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Csharp/ChefsAndDishes/Migrations/20210819051534_Migration2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChefsAndDishes.Migrations
+{
+    public partial class Migration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Chef",
+                table: "Dishes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Chef",
+                table: "Dishes",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false);
+        }
+    }
+}
