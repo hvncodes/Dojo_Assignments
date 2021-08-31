9abaf40f1c2575dd3ded3efc691dd5a6761c4aaa
John Nguyen <johnhvn94@gmail.com>
2021-08-31 00:20:45 -0700

    Destructing

--- MERN/app.js
+++ MERN/app.js
@@ -1,139 +1,48 @@
-console.log(example);
-let example = "I'm the example!";    
-// let not hoisted, throws error
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)      // Tesla
+console.log(otherRandomCar) // Mercedes 
 
-console.log(hello);
-var hello = 'world';
-// var hello;
-// console.log(hello); // hello is undefined
-// hello = 'world'
-
-var needle = 'haystack';
-test();
-function test(){
-    var needle = 'magnet';
-    console.log(needle);
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
 }
-// var needle = 'haystack'; // needle hoisted and declared
-// test(); // test is undefined
-// function test(){
-//     var needle = 'magnet'; // needle hoisted to top of test() method and declared as magnet
-//     console.log(needle); // logs magnet instead of haystack
-// }
-var brendan = 'super cool';
-function print(){
-    brendan = 'only okay';
-    console.log(brendan);
+const { name: otherName } = employee;
+//Predict the output
+// console.log(name);      // undefined
+console.log(otherName); // Elon
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
 }
-console.log(brendan);
+const password = '12345';
+const { password: hashedPassword } = person;
+//Predict the output
+console.log(password);        // 12345
+console.log(hashedPassword);  // undefined
 
-// var brendan; // hoisted
-// function print(){
-//     brendan = 'only okay'; // hoisted within print
-//     console.log(brendan); // doesn't get called since print is never called
-// }
-// brendan = 'super cool'; // global
-// console.log(brendan); // prints 'super cool'
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;       // 2
+const [,,,second] = numbers;    // 5
+const [,,,,,,,,third] = numbers;// 2
+//Predict the output
+console.log(first == second);   // false
+console.log(first == third);    // true
 
-var food = 'chicken';
-console.log(food);
-eat();
-function eat(){
-    food = 'half-chicken';
-    console.log(food);
-    var food = 'gone';
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
 }
-
-// var food;                     // hoisted
-// food = 'chicken';             // declared
-// console.log(food);            // prints chicken
-// eat();                        ~~// called before defined?~~
-// function eat(){               // hoisted
-//     var food;                 // hoisted
-//     food = 'half-chicken';    // declared
-//     console.log(food);        // prints 'half-chicken'
-//     food = 'gone';            // declared
-// }
-// food = 'chicken';  // global
-
-mean();
-console.log(food);
-var mean = function() {
-    food = "chicken";
-    console.log(food);
-    var food = "fish";
-    console.log(food);
-}
-console.log(food);
-// var mean;               // hoisted
-// mean();                 // mean is not a function
-// console.log(food);      // code already broke, food isn't a variable
-// mean = function() {
-//     var food
-//     food = "chicken";
-//     console.log(food);
-//     food = "fish";
-//     console.log(food);
-// }
-// console.log(food);
-
-
-console.log(genre);
-var genre = "disco";
-rewind();
-function rewind() {
-    genre = "rock";
-    console.log(genre);
-    var genre = "r&b";
-    console.log(genre);
-}
-console.log(genre);
-
-// var genre;                  // hoisted
-// console.log(genre);         // undefined
-// genre = "disco";            // assigned
-// rewind();                   // undefined
-// function rewind() {
-//     var genre;              // hoisted within rewind()
-//     genre = "rock";         // assigned
-//     console.log(genre);     // prints "rock"
-//     genre = "r&b";          // re-assigned
-//     console.log(genre);     // prints "r&b"
-// }
-// console.log(genre);         // prints "disco/"
-
-dojo = "san jose";
-console.log(dojo);
-learn();
-function learn() {
-    dojo = "seattle";
-    console.log(dojo);
-    var dojo = "burbank";
-    console.log(dojo);
-}
-console.log(dojo);
-
-// dojo = "san jose";            // assigned
-// console.log(dojo);            // prints "san jose"
-// learn();                      // ~~undefined~~
-// function learn() {
-//     var dojo                  // hoisted
-//     dojo = "seattle";         // assigned
-//     console.log(dojo);        // prints "seattle"
-//     dojo = "burbank";         // re-assigned
-//     console.log(dojo);        // prints "burbank"
-// }
-// console.log(dojo);            // prints "san jose" again
-
-// ACTUAL
-// function learn() {
-//     var dojo
-//     dojo = "seattle";
-//     console.log(dojo);
-//     dojo = "burbank";
-//     console.log(dojo);
-// }
-// dojo = "san jose";
-// console.log(dojo);
-// learn();
-// console.log(dojo);
+const { key } = lastTest;           // 'value'
+const { secondKey } = lastTest;     // 1
+const [ ,willThisWork] = secondKey; // 5
+//Predict the output
+console.log(key);                   // 'value'
+console.log(secondKey);             // ~~1~~            // [1, 5, 1, 8, 3, 3]
+console.log(secondKey[0]);          // ~~undefined~~    // 1
+console.log(willThisWork);          // 5
