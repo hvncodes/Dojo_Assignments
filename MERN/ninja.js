50ae1dc7f833f8f36b42971fc8a0e1c86e1c853d
John Nguyen <johnhvn94@gmail.com>
2021-08-31 18:56:24 -0700

    added Sensei class that extends Ninja

--- MERN/ninja.js
+++ MERN/ninja.js
@@ -28,3 +28,28 @@ const ninja1 = new Ninja("Hyabusa", 10);
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
+
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name, 200);
+        this.wisdom = 10;
+    }
+
+    speakWisdom() {
+        super.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.");
+    }
+    
+    // Sensei's own showStats method.
+    showStats() {
+        // "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
+        console.log(`Name: ${ this.name }, Health: ${ this.health }, Speed: ${ this.speed }, Strength: ${ this.strength }`);
+    }
+}
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
