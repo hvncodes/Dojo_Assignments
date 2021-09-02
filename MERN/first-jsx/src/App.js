142ab41617714b02c2de779cb7f57f4c086e3726
John Nguyen <johnhvn94@gmail.com>
2021-09-02 02:36:21 -0700

    comments on syntax

--- MERN/first-jsx/src/App.js
+++ MERN/first-jsx/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 // import MyFirstComponent from './components/MyFirstComponent';
 import PersonCard from './components/PersonCard';
-
-var peopleArr =[
-  {"firstName":"Jane", "lastName":"Doe", "age":45, "hairColor":"Black"},
-  {"firstName":"John", "lastName":"Smith", "age":88, "hairColor":"Brown"},
-  {"firstName":"Millard", "lastName":"Fillmore", "age":50, "hairColor":"Brown"},
-  {"firstName":"Maria", "lastName":"Smith", "age":62, "hairColor":"Brown"}
+// unquoted vs quoted keys:
+// https://stackoverflow.com/questions/4348478/what-is-the-difference-between-object-keys-with-quotes-and-without-quotes
+const peopleArr =[
+  { "firstName": "Jane", "lastName": "Doe", "age":45, "hairColor": "Black" },
+  { "firstName": "John", "lastName": "Smith", "age":88, "hairColor": "Brown" },
+  { "firstName": "Millard", "lastName": "Fillmore", "age":50, "hairColor": "Brown" },
+  { "firstName": "Maria", "lastName": "Smith", "age":62, "hairColor": "Brown" }
 ]
 
 function App() {
@@ -17,11 +18,11 @@ function App() {
       <PersonCard firstName="John" lastName="Smith" age="88" hairColor="Brown"/>
       <PersonCard firstName="Millard" lastName="Fillmore" age="50" hairColor="Brown"/>
       <PersonCard firstName="Maria" lastName="Smith" age="62" hairColor="Brown"/> */}
-      {
-        peopleArr.map(person => {
-          return <PersonCard firstName={person.firstName} lastName={person.lastName} age={person.age} hairColor={person.hairColor} />
-        })
-      }
+
+      {/* https://www.pluralsight.com/guides/how-to-use-the-map()-function-to-export-javascript-in-react */}
+      {peopleArr.map(person => {
+        return <PersonCard firstName={person.firstName} lastName={person.lastName} age={person.age} hairColor={person.hairColor} />
+      })}
     </div>
   );
 }
