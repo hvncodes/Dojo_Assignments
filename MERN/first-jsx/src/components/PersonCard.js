f7c5b1f6af9842322b43cd6a330ecbce0174dea1
John Nguyen <johnhvn94@gmail.com>
2021-09-02 21:56:42 -0700

    Synthetic Event w/ Props & State

--- MERN/first-jsx/src/components/PersonCard.js
+++ MERN/first-jsx/src/components/PersonCard.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import classes from './PersonCard.module.css';
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: props.person.age
+        };
+    }
     render() {
-        const { firstName, lastName, age, hairColor } = this.props.person;
+        const { firstName, lastName, hairColor } = this.props.person;
         return (
             <div className={classes.card}>
                 <div className={classes.content}>
                     <h1>{ lastName }, { firstName }</h1>
-                    <p>Age: { age }</p>
+                    <p>Age: { this.state.age }</p>
                     <p>Hair Color: { hairColor }</p>
+                    <button onClick={ this.agePlusOne }>Birthday Button for { firstName } { lastName }</button>
                 </div>
             </div>
         );
+    }
+
+    agePlusOne = () => {
+        //parseInt(this.state.age) + 1;
+        // let currentAgePlusOne = this.state.age + 1
+        this.setState({ age: this.state.age + 1 });
     }
 }
 
